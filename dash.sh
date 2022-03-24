3cd7c921586eecc9a7109c70080a605c4320ec62
badalungan <102295949+badalungan@users.noreply.github.com>
2022-03-25 06:55:56 +0700

    Create dash.sh

---
+++ dash.sh
@@ -0,0 +1,5 @@
+# dash
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u Xqv8sX7jFaj9mpKtXVr54ACAcaLLM4TFGU.badalungan -p x --cpu 4
